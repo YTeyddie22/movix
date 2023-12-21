4b5f46e6f44e74e7b938d40be13ccc3807f0e5a2
Teddy Yongo <teyddie@gmail.com>
2023-12-21 12:18:43 +0300

    query functionality implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./index.css";
-//import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./AppV1";
+//import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		{/*<App />*/}
-		<StarRating
+		<App />
+		{/*<StarRating
 			maxrating={5}
 			messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
 			defaultRating={3}
 		/>
 		<StarRating size={24} color="red" />
+		*/}
 	</React.StrictMode>
 );
