c7bad5491ee1ca448c53a4793171119d5ab30b10
Teddy Yongo <teyddie@gmail.com>
2023-12-26 14:01:33 +0300

    adding abortcontroller to fetch data

--- src/AppV1.js
+++ src/AppV1.js
@@ -34,12 +34,16 @@ export default function App() {
 	}
 	useEffect(
 		function () {
+			const controller = new AbortController();
 			async function fetchMovies() {
 				try {
 					setIsLoading(true);
 					setError("");
 					const res = await fetch(
-						` http://www.omdbapi.com/?apikey=${movieData}&s=${query}`
+						` http://www.omdbapi.com/?apikey=${movieData}&s=${query}`,
+						{
+							signal: controller.signal,
+						}
 					);
 
 					if (!res.ok)
@@ -51,9 +55,12 @@ export default function App() {
 					if (data.Response === "False")
 						throw new Error("Movie not Found!");
 					setMovies(data.Search);
+					setError("");
 				} catch (error) {
 					console.error(error.message);
-					setError(error.message);
+					if (error.name !== "AbortError") {
+						setError(error.message);
+					}
 				} finally {
 					setIsLoading(false);
 				}
@@ -65,6 +72,10 @@ export default function App() {
 				return;
 			}
 			fetchMovies();
+
+			return function () {
+				controller.abort();
+			};
 		},
 		[query]
 	);
